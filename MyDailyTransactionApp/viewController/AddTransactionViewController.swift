0c7970ef4e58f3872cb6c52cd3b59ec4ad93f6ab
urayfajri <uraywidiansyah88@gmail.com>
2022-04-27 15:15:44 +0700

    hide tab bar when on push

---
+++ MyDailyTransactionApp/viewController/AddTransactionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddTransactionViewController.swift
+//  MyDailyTransactionApp
+//
+//  Created by Uray Muhamad Noor Fajri Widiansyah on 27/04/22.
+//
+
+import UIKit
+
+class AddTransactionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
