0301d3e9b482800debb51f88f402ed37bd5c5848
urayfajri <uraywidiansyah88@gmail.com>
2022-04-29 00:15:49 +0700

    income and expense adding

---
+++ MyDailyTransactionApp/viewController/AddExpenseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddExpenseViewController.swift
+//  MyDailyTransactionApp
+//
+//  Created by Uray Muhamad Noor Fajri Widiansyah on 28/04/22.
+//
+
+import UIKit
+
+class AddExpenseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
