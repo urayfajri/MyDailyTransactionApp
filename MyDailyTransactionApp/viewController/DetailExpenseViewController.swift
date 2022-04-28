f7ecd6967159c332e97044812e040bffcc3cf8b9
urayfajri <uraywidiansyah88@gmail.com>
2022-04-28 23:40:54 +0700

    creating detail income and expense storyboard

---
+++ MyDailyTransactionApp/viewController/DetailExpenseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailExpenseViewController.swift
+//  MyDailyTransactionApp
+//
+//  Created by Uray Muhamad Noor Fajri Widiansyah on 28/04/22.
+//
+
+import UIKit
+
+class DetailExpenseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
