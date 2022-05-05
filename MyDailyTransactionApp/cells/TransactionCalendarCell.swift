18f2c1cd595b3c9ca76e8a10daff1fe9c1862316
urayfajri <uraywidiansyah88@gmail.com>
2022-05-05 16:55:12 +0700

    add collection view on calendar view

---
+++ MyDailyTransactionApp/cells/TransactionCalendarCell.swift
@@ -0,0 +1,15 @@
+//
+//  TransactionCalendarCell.swift
+//  MyDailyTransactionApp
+//
+//  Created by Uray Muhamad Noor Fajri Widiansyah on 05/05/22.
+//
+
+import UIKit
+
+class TransactionCalendarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var transactionCalendarName: UILabel!
+    @IBOutlet weak var transactionCalendarBudget: UILabel!
+    @IBOutlet weak var transactionCalendarDate: UILabel!
+}
--- MyDailyTransactionApp/viewController/CalenderViewController.swift
+++ MyDailyTransactionApp/viewController/CalenderViewController.swift
@@ -7,21 +7,60 @@
 
 import FSCalendar
 import UIKit
+import CoreData
 
-class CalenderViewController: UIViewController, FSCalendarDelegate {
-    
+class CalenderViewController: UIViewController, FSCalendarDelegate, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     @IBOutlet weak var calendar: FSCalendar!
+    @IBOutlet weak var transactionCollectionView: UICollectionView!
+    @IBOutlet weak var emptyTransactionLabel: UILabel!
     
     private var transactions = [Transaction]()
+    private var transactionCalendars = [Transaction]()
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        getAllTransactions()
+        
         calendar.delegate = self
+        
+        transactionCollectionView.delegate = self
+        transactionCollectionView.dataSource = self
+        
+        let transactionLayout = UICollectionViewFlowLayout()
+        transactionLayout.scrollDirection = .vertical
+        transactionLayout.minimumLineSpacing = 10
+        transactionLayout.minimumInteritemSpacing = 10
+        transactionCollectionView.setCollectionViewLayout(transactionLayout, animated: true)
 
+    }
+    
+    func getAllTransactions(){
+        // fetch the data from core data in the table view
+        let fetchRequest: NSFetchRequest<Transaction> = Transaction.fetchRequest()
+        
+        do
+        {
+            transactions = try context.fetch(fetchRequest)
+                    
+            if(transactions.isEmpty)
+            {
+//                hintLabel.isHidden = true
+//                emptyList.isHidden = false
+//                emptyList.text = "You haven’t create any transaction\nStart create one now!"
+//                emptyList.textColor = .gray
+//                emptyList.textAlignment = .center
+//                emptyList.frame = CGRect(x: 0, y: view.frame.size.height / 2, width: view.frame.size.width, height: 100)
+//                view.addSubview(emptyList)
+            }
+        }
+        catch
+        {
+            //error
+        }
     }
     
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
@@ -31,6 +70,35 @@ class CalenderViewController: UIViewController, FSCalendarDelegate {
         let selectedDate = dateFormat.string(from: date)
         
         print("\(selectedDate)")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 1.0, left: 1.0, bottom: 1.0, right: 1.0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let lay = collectionViewLayout as! UICollectionViewFlowLayout
+        let widthPerItem = collectionView.frame.width - lay.minimumInteritemSpacing
+        return CGSize(width: widthPerItem, height: 80)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return transactions.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "transaction_calendar_cell", for: indexPath) as! TransactionCalendarCell
+        let transaction = transactions[indexPath.row]
+        
+        cell.backgroundColor = .systemGray6
+        cell.layer.cornerRadius = 15
+        
+        cell.transactionCalendarName.text = transaction.transactionName
+        cell.transactionCalendarBudget.text = transaction.transactionDescription
+        cell.transactionCalendarDate.text = transaction.transactionStatus
+        
+        return cell
     }
     
     @IBAction func addButtonTapped(_ sender: Any) {
@@ -66,5 +134,4 @@ class CalenderViewController: UIViewController, FSCalendarDelegate {
             }
         }
     }
-    
 }
